914ab2938fbf029eb8f6245415a71e2812dcd308
cs-sdran <qiulan978@gmail.com>
2026-05-17 16:46:39 +0800

    5.17

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,4 @@
+import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -596,3 +597,47 @@ class Solution {
     }
 }
 */
+
+/*
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/*
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty())
+        {
+            int size=queue.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++)
+            {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if(node.left!=null)
+                    queue.add(node.left);
+                if(node.right!=null)
+                    queue.add(node.right);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}*/
